fc3b5b1038f034c90e5ffd97d1d29c455cdf73f9
Ahmed Abdulrahim <medo1562003@gmail.com>
2024-09-17 21:21:06 +0300

    minor grammar fixes

--- MVC_Session_1/MVC_Session_1/obj/Debug/net8.0/MVC_Session_1.AssemblyInfo.cs
+++ MVC_Session_1/MVC_Session_1/obj/Debug/net8.0/MVC_Session_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_Session_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a000102b3881db92a66f9c2b08de9ba486819e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_Session_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_Session_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
